63b96631ecfa6cbfbec8e00c815ca28dd3a85ef3
BrinkOfSecondBailout <nguyenduongpa@gmail.com>
2025-11-05 18:18:37 -0800

    first commit

---
+++ src/bin/main.rs
@@ -0,0 +1,47 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+use std::{thread, fs};
+use std::time::Duration;
+use webapp_r::ThreadPool;
+
+const LOCAL_HOST: &str = "127.0.0.1:7878";
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+    let (status, filename) = 
+        if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK", "index.html")
+        } else if buffer.starts_with(sleep) {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        } else {
+            ("HTTP/1.1 400 NOT FOUND", "404.html")
+        };
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status,
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+fn main() {
+    let listener = TcpListener::bind(LOCAL_HOST).unwrap();
+    let pool = ThreadPool::new(4);
+    println!("Listening!");
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Incoming connection");
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
